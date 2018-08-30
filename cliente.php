0dd4216388be18bda184dc0294b6fb05d356d34d
jbgonzalezsia <jb.gonzalezsia@gmail.com>
2018-08-30 18:40:56 -0500

    web service add code to cliente
    se le agregaron parametros al cliente que va consumir el web service

--- cliente.php
+++ cliente.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'lib/nusoap.php';
 
-$cliente = new nusoap_client("http://nusoapwebservice-one.local/servicio.php")
+$cliente = new nusoap_client("http://nusoapwebservice-one.local/servicio.php", false);
+
+$num1 = 78;
+$num2 = 88;
+
+$parametros = array('num1'=>$num1, 'num2'=>$num2);
+$respuesta = cliente->call('MiFuncion',$parametros);
 ?>
+
